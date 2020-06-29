b50e64a944ac4545de21d26c2f036330eae2465f
luckydh <sajdh65@gmail.com>
2020-06-28 22:54:05 -0500

    Updates

---
+++ precache-manifest.b7058ed1c993debf56e1d4e55fbc98af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e36ff9cff3591ce786a5029c8076a3",
+    "url": "/monsters-rolo/index.html"
+  },
+  {
+    "revision": "89ab018f1ef36d3ef54e",
+    "url": "/monsters-rolo/static/css/main.38dc7c37.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolo/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolo/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ab018f1ef36d3ef54e",
+    "url": "/monsters-rolo/static/js/main.319c9ae3.chunk.js"
+  },
+  {
+    "revision": "c81157c5895836a9db3c",
+    "url": "/monsters-rolo/static/js/runtime-main.1b3a0b72.js"
+  }
+]);
